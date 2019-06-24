50ca996446ce0a72860d778c6a67e4df2f9cd578
weiwinter <837144154@qq.com>
2019-06-24 18:10:43 +0800

    20190624-ch8_2

---
+++ ch8_2/src/main/java/com/wisely/ch8_2/Ch82Application.java
@@ -0,0 +1,13 @@
+package com.wisely.ch8_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch82Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch82Application.class, args);
+	}
+
+}
---
+++ ch8_2/src/main/java/com/wisely/dao/PersonRepository.java
@@ -0,0 +1,22 @@
+package com.wisely.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.wisely.domain.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+	List<Person> findByAddress(String name);
+	
+	Person findByNameAndAddress(String name,String address);
+	
+	@Query("select p from Person p where p.name=:name and p.address=:address")
+	Person withNameAndAddressQuery(@Param("name")String name,@Param("address")String address);
+	
+	List<Person> withNameAndAddressNamedQuery(String name,String address);
+	
+}
